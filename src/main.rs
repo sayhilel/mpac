329a67608a27d534916db8121f223abaa9674f48
Sahil Sinha <dev.sahilsinha@gmail.com>
2024-05-16 16:09:30 +0530

    Seperated repo & repos // too much OOP?

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,19 @@
 // Modules
 mod cli;
 mod repo;
+mod repos;
 
 // Use
 use clap::Parser;
 use cli::Args;
 use git2::Repository;
 
-use crate::repo::Repos;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = cli::Args::parse();
     println!("Command::{}, Flag::{}", args.command, args.flag);
 
     //Test Object
-    let repos = repo::new().unwrap();
+    let repos = repos::new().unwrap();
 
     repos.list_repos();
 
---
+++ src/repos.rs
@@ -0,0 +1,48 @@
+use crate::repo::Repo;
+
+use std::path::Path;
+use std::{
+    fs::{File, OpenOptions},
+    io::{self, prelude, BufRead, BufReader, Error},
+};
+
+// Might help later
+pub struct Repos {
+    repos: Vec<Repo>,
+}
+
+// Implement functions for repos here
+pub fn new() -> Result<Repos, Error> {
+    // Open the file
+    let path = Path::new("/home/croxymoc/Documents/Projects/manpac/target/debug/repos.txt"); // Very system dependant // Remember to change
+    let repo_file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(path)?;
+
+    // Load ; REPLACE WITH JSON LATER
+    let mut reader = BufReader::new(repo_file);
+    let mut repos_new = Repos { repos: vec![] };
+    // Very ugly and impractical but we ball for now
+    for repo_path in reader.lines() {
+        repos_new.repos.push(Repo { path: repo_path? });
+    }
+
+    Ok(repos_new)
+}
+
+//
+impl Repos {
+    pub fn list_repos(&self) {
+        match self.repos.len() {
+            0 => print!("NO REPOS --CHANGE-ME\n"),
+            _ => {
+                for repo in self.repos.iter() {
+                    print!("{}\n", repo.path);
+                }
+            }
+        }
+    }
+}
+//pub fn update_repos(repo) -> Result<(), Error> {}
